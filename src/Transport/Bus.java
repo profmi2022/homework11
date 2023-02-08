dbebb23f7e76d5a748e967121314e09036b2636c
profmi2022 <profmi2022@gmail.com>
2023-02-08 20:55:27 +0600

    ДЗ "ООП" Часть 3 выполнено

---
+++ src/Transport/Bus.java
@@ -0,0 +1,11 @@
+package Transport;
+
+public class Bus extends Transport {
+
+    public Bus(String brand, String model, String color, int productionYear, String productionCountry, int maxSpeed){
+        super(brand, model, color, productionYear, productionCountry, maxSpeed);
+    }
+
+
+
+}
